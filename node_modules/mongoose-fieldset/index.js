d93777d6125f3875b6075798d17320c7d286b5c0
Richa Goel <Richagoel@Richa-Goels-MacBook.local>
2014-07-09 13:25:52 +0530

    Example  uploaded for mongoose fieldset plugin

---
+++ node_modules/mongoose-fieldset/index.js
@@ -0,0 +1,53 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nitin
+ * Date: 1/7/14
+ * Time: 11:47 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+var mongoose = require('mongoose');
+//var fieldsSetPlugin = require('mongoose-fieldset');
+var Schema = mongoose.Schema;
+
+/*Describe a Schema*/
+var PersonSchema = new Schema({
+    'time' : {'type': Date, 'index': true, 'alias': 'timestamp', 'fieldset':'private'},
+    'name' : {'type' : String, 'alias': 'name1','fieldset':'public'},
+    'surname' : {'type' : String, 'alias': 'surname1','fieldset':'private'},
+    'profile' : {
+        'address' : {'type' : String, 'alias': 'profile.address1','fieldset':'public'},
+        'phone-number': {'type' : String, 'alias': 'profile.phone-number1', 'fieldset':'private'}
+    }
+});
+
+
+
+/*Add field alias plugin*/
+// PersonSchema.plugin(fieldsAliasPlugin);
+mongoose.connect('mongodb://localhost/myapp');
+var Person =  mongoose.model('Person', PersonSchema);
+//mongoose.model('Person');
+var person = new Person({
+    'timestamp'	: new Date(),
+    'name'		: 'Jhon',
+    'surname'	: 'Doe',
+    'profile.address': 'Rue de Morgane',
+    'profile.phone-number': '051-123456 78'
+});
+
+// person.save();
+
+console.log( "---------------get Public Fields---------------------");
+console.log(person.toObject({fieldSet:'public', transform:true}));
+
+console.log( "---------------get Private Fields-------------------- ");
+console.log(person.toObject({fieldSet:'private', transform:true}));
+
+console.log('---------------Person Public FieldSet-------------');
+Person.findOne({name:'Jhon'},Person.getFieldNamesForSet('public')).exec(function(err,objs){
+    console.log( objs);
+    console.log( "-------------------------------------------------- ");
+});
+
